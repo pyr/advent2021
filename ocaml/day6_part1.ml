26b5a4d8c61707662e667d11bb021318aeb2e197
Pierre-Yves Ritschard <pyr@spootnik.org>
2021-12-08 04:40:29 +0100

    day6: simplify ocaml too

--- ocaml/day6_part1.ml
+++ ocaml/day6_part1.ml
@@ -1,27 +1,42 @@
-(** once opam is installed, run with coretop day6_part1.ml **)
+(** once opam is installed, run with coretop day6_part2.ml **)
 (** https://adventofcode.com/2021/day/6 **)
 #require "line_oriented"
 
 open Printf
 module L = Line_oriented
+module Counters = Map.Make(Int)
 
 (** parsing **)
 let parse_timers s =
   s |> String.split_on_char ',' |> List.map int_of_string
 
 (** solving **)
-let tick_fish x =
-  if x == 0 then [6; 8] else [x - 1]
+let tick k v m =
+  let updater = function
+    | (Some x) -> Some (x + v)
+    | None     -> Some v in
 
-let rec solver fishes days =
-  if days > 0 then
-    let fishes' = List.concat_map tick_fish fishes in
-    solver fishes' (days - 1)
+  if k == 0 then
+    m |> Counters.update 6 updater |> Counters.update 8 updater
   else
-    List.length fishes
+    Counters.update (k - 1) updater m
+
+let frequencies fishes =
+  let updater = function
+    | (Some x) -> Some (x + 1)
+    | None     -> Some 1 in
+  List.fold_left (fun acc v -> Counters.update v updater acc) Counters.empty fishes
+
+let solver days input =
+  let rec ticker day fishes =
+    if day < days then
+      ticker (day + 1) (Counters.fold tick fishes Counters.empty)
+    else
+      Counters.fold (fun _ v acc -> v + acc) fishes 0 in
+  ticker 0 (frequencies input)
 
 (** main **)
 let () =
   let fishes = "../resources/input6.txt" |> L.lines_of_file |> List.hd |> parse_timers in
-  let fish_count = solver fishes 80 in
+  let fish_count = solver 80 fishes in
   printf "day6_part1: %d\n" fish_count
--- ocaml/day6_part2.ml
+++ ocaml/day6_part2.ml
@@ -4,51 +4,36 @@
 
 open Printf
 module L = Line_oriented
-
-(** globals **)
-let fish_counts = Hashtbl.create 128
+module Counters = Map.Make(Int)
 
 (** parsing **)
 let parse_timers s =
   s |> String.split_on_char ',' |> List.map int_of_string
 
 (** solving **)
-let can_generate (value,days) = days > value
+let tick k v m =
+  let updater = function
+    | (Some x) -> Some (x + v)
+    | None     -> Some v in
 
-let generations (value,days) =
-  if can_generate (value,days) then
-    let shift = 6 - value in
-    let days' = days + shift in
-    let num_gens = (days' / 7) - 1 in
-    let shift' = days' mod 7 in
-    let rec days_of_gens res counter =
-      if counter >= 0 then
-        days_of_gens ((shift' + (counter * 7))::res) (counter - 1)
-      else
-        List.map (fun day -> (8,day)) res in
-    days_of_gens [] num_gens
+  if k == 0 then
+    m |> Counters.update 6 updater |> Counters.update 8 updater
   else
-    []
+    Counters.update (k - 1) updater m
 
-let rec count_fish_cached fish =
-  let count_fish fish =
-    let gens = generations fish in
-    let subs = List.filter can_generate gens in
-    let subcounts = List.map count_fish_cached subs in
-    let adder x y = x + y in
-    let count = List.fold_left adder (List.length gens) subcounts in
-    Hashtbl.replace fish_counts fish count;
-    count in
-  let count = match Hashtbl.find_opt fish_counts fish with
-    | (Some count) -> count
-    | None -> count_fish fish in
-  count
+let frequencies fishes =
+  let updater = function
+    | (Some x) -> Some (x + 1)
+    | None     -> Some 1 in
+  List.fold_left (fun acc v -> Counters.update v updater acc) Counters.empty fishes
 
-let solver days fishes =
-  fishes
-  |> List.map (fun value -> (value,days))
-  |> List.map count_fish_cached
-  |> List.fold_left (fun x y -> x + y) (List.length fishes)
+let solver days input =
+  let rec ticker day fishes =
+    if day < days then
+      ticker (day + 1) (Counters.fold tick fishes Counters.empty)
+    else
+      Counters.fold (fun _ v acc -> v + acc) fishes 0 in
+  ticker 0 (frequencies input)
 
 (** main **)
 let () =
