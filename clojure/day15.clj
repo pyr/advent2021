1210c394af650a06b0e64982215fc4c6d7611872
Pierre-Yves Ritschard <pyr@spootnik.org>
2021-12-15 13:56:35 +0100

    day15: clojure

---
+++ clojure/day15.clj
@@ -0,0 +1,79 @@
+(ns day15
+  (:require [clojure.java.io :as io]))
+
+(defn- parse-row
+  [[y row]]
+  (for [[x c] (map-indexed vector row)]
+    [[x y] (- (int c) (int \0))]))
+
+(defn input
+  [path]
+  (let [lines (-> path io/resource io/reader line-seq)
+        size  (count (first lines))]
+    {:size    size
+     :target  [(dec size) (dec size)]
+     :grid    (into {}
+                    (comp (map-indexed vector)
+                          (mapcat parse-row))
+                    lines)}))
+(defn dijkstra
+  [adjacents]
+  (let [minpair #(first (sort-by val %))]
+    (loop [q    {[0 0] 0}
+           seen {}]
+      (if-let [[pos cost] (minpair q)]
+        (let [dist (reduce
+                    #(assoc %1 (key %2) (+ cost (val %2))) {}
+                    (eduction (remove #(contains? seen (key %))) (adjacents pos)))]
+          (recur (merge-with min (dissoc q pos) dist)
+                 (assoc seen pos cost)))
+        seen))))
+
+(defn adjacents-fn
+  [{:keys [size grid]}]
+  (fn [[x y]]
+    (let [invalid? #(or (= % [0 0]) (some neg? %) (some (partial <= size) %))]
+      (into {}
+            (comp
+             (remove invalid?)
+             (map (juxt identity (partial get grid))))
+            [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]]))))
+
+(defn part1
+  [path]
+  (let [board (input path)]
+    (get (dijkstra (adjacents-fn board)) (:target board))))
+
+(defn expand-cells
+  [grid size]
+  (let [increments (map (partial * size) (range 5))]
+    (for [x    increments
+          y    increments
+          xoff (range size)
+          yoff (range size)]
+      [[(+ x xoff) (+ y yoff)]
+       (let [v (+ (get grid [xoff yoff]) (quot x size) (quot y size))]
+         (cond-> v (> v 9) (- 9)))])))
+
+(defn expand-board
+  [{:keys [size] :as board}]
+  (let [size (* size 5)]
+    (-> board
+        (assoc :size size)
+        (assoc :target [(dec size) (dec size)])
+        (assoc :grid (into {} (expand-cells (:grid board) (:size board)))))))
+
+(defn part2
+  [path]
+  (let [board (expand-board (input path))]
+    (get (dijkstra (adjacents-fn board)) (:target board))))
+
+(comment
+
+  (part1 "input15.txt")
+  ;; A bit slow (~ 1 minute on my machine, 20s with Xmx8192)
+  (time
+   (part2 "input15.txt"))
+
+
+  )
