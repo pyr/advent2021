6773b48a07f3d1e52c3c84e785e4711cf15d9733
Pierre-Yves Ritschard <pyr@spootnik.org>
2021-12-15 20:31:17 +0100

    day15: make it fast

--- clojure/day15.clj
+++ clojure/day15.clj
@@ -3,45 +3,81 @@
 
 (defn- parse-row
   [[y row]]
-  (for [[x c] (map-indexed vector row)]
-    [[x y] (- (int c) (int \0))]))
+  (for [[x c] (map-indexed vector row)] [[x y] (- (int c) (int \0))]))
 
 (defn input
   [path]
   (let [lines (-> path io/resource io/reader line-seq)
         size  (count (first lines))]
-    {:size    size
-     :target  [(dec size) (dec size)]
-     :grid    (into {}
-                    (comp (map-indexed vector)
-                          (mapcat parse-row))
-                    lines)}))
-(defn dijkstra
-  [adjacents]
-  (let [minpair #(first (sort-by val %))]
-    (loop [q    {[0 0] 0}
-           seen {}]
-      (if-let [[pos cost] (minpair q)]
-        (recur (reduce #(update %1 (key %2) (fnil min Long/MAX_VALUE) (+ cost (val %2)))
-                       (dissoc q pos)
-                       (eduction (remove #(contains? seen (key %))) (adjacents pos)))
-               (assoc seen pos cost))
-        seen))))
+    {:size   size
+     :target [(dec size) (dec size)]
+     :grid   (into {} (comp (map-indexed vector) (mapcat parse-row)) lines)}))
 
 (defn adjacents-fn
   [{:keys [size grid]}]
   (fn [[x y]]
     (let [invalid? #(or (= % [0 0]) (some neg? %) (some (partial <= size) %))]
-      (into {}
-            (comp
-             (remove invalid?)
-             (map (juxt identity (partial get grid))))
-            [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]]))))
+      (eduction
+       (comp
+        (remove invalid?)
+        (map (juxt identity (partial get grid))))
+       [[(dec x) y] [(inc x) y] [x (dec y)] [x (inc y)]]))))
+
+(defn dijkstra
+  [adjacents]
+  (let [minpair #(first (sort-by val %))]
+    (loop [q    {[0 0] 0}
+           seen {}]
+      ;; This is the slow part, since we use a
+      (if-let [[pos cost] (minpair q)]
+        ;; The following is a bit more contrived than what we'd get with a simple
+        ;; ->> but it tries to speed things up. I'm trying to keep with plain
+        ;; Clojure and no additional dependencies, otherwise c.d.priority-map would
+        ;; likely give a good speed boost here
+        (recur (reduce #(update %1 (first %2) (fnil min Long/MAX_VALUE)
+                                (+ cost (last %2)))
+                       (dissoc q pos)
+                       (eduction (remove #(contains? seen (first %))) (adjacents pos)))
+               (assoc seen pos cost))
+        seen))))
+
+(defn fast-dijkstra
+  [adjacents]
+  ;; Ugly but way faster version of dijkstra. The main problem with the
+  ;; version above is the constant sorting that gets done on the same
+  ;; dataset.
+  ;;
+  ;; What this version does instead is hold on to two values: a map and
+  ;; a sorted set. The map allows fast lookups by position, and the sorted
+  ;; set allows popping the lowest available cost path quickly.
+  ;;
+  ;; As hinted to above, this is more or less what c.d.priority-map does.
+  ;;
+  ;; This version drops down to ~ 5s from the 50s for the large input
+  (loop [{:keys [qs] :as q} {:qm {[0 0] 0} :qs (sorted-set [0 [0 0]])}
+         seen               {}]
+    (if-let [[cost pos] (first qs)]
+      (recur (reduce (fn [{:keys [qm] :as q} [k add-cost]]
+                       (if-let [prev (get qm k)]
+                         (let [v       (min prev (+ cost add-cost))
+                               update? (= prev v)]
+                           (cond-> (assoc-in q [:qm k] v)
+                             update? (-> (update :qs disj [prev k])
+                                         (update :qs conj [v k]))))
+                         (-> q
+                             (assoc-in [:qm k] (+ cost add-cost))
+                             (update :qs conj [(+ cost add-cost) k]))))
+                     (-> q
+                         (update :qm dissoc pos)
+                         (update :qs disj [cost pos]))
+                     (eduction (remove #(contains? seen (first %))) (adjacents pos)))
+             (assoc seen pos cost))
+      seen)))
 
 (defn part1
   [path]
   (let [board (input path)]
-    (get (dijkstra (adjacents-fn board)) (:target board))))
+    (get (fast-dijkstra (adjacents-fn board)) (:target board))))
 
 (defn expand-cells
   [grid size]
@@ -65,16 +101,14 @@
 (defn part2
   [path]
   (let [board (expand-board (input path))]
-    (get (dijkstra (adjacents-fn board)) (:target board))))
+    (get (fast-dijkstra (adjacents-fn board)) (:target board))))
 
 (comment
 
   (time
    (part1 "input15.txt"))
 
-  ;; A bit slow (~ 50s minute on my machine, 20s with Xmx8192)
   (time
    (part2 "input15.txt"))
-
 
   )
